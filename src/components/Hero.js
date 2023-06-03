61df09ec3615cc134d67c0f231fc82f20ba1d38e
Julija Jelicanin <julijajelicanin@192.168.1.8>
2023-06-03 16:19:10 +0200

    Added Calendly to schedule meetings

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,37 @@
 import React from "react";
 import './Hero.css'
 import heroImage from'.././assets/hero-image.png';
+import { useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { PopupButton, PopupWidget} from "react-calendly"
+
 
 function Hero() {
+    // Popup Widget
+    // const PopupComponent = () => {
+    //     return (
+    //       <div className="popup-widget">
+    //         <PopupWidget
+    //           url="https://calendly.com/uxjulija/kickoff-meeting" 
+    //           rootElement={document.getElementById('root')}
+    //           text="Schedule Time with me"
+    //           textColor="#ffffff"
+    //           color="#319795"
+    //         />
+    //       </div>
+    //     );
+    //   }
+      const PopupComponent = () => {
+        return (
+          <div className="popup-button">
+            <PopupButton className="button primary"
+              url="https://calendly.com/uxjulija/kickoff-meeting" 
+              rootElement={document.getElementById('root')}
+              text="Schedule a call"
+            />
+          </div>
+        );
+      }
     return (
         <section className="heroSection bgGradient">
             <div className="container heroContainer">
@@ -13,8 +42,8 @@ function Hero() {
                         user-friendly digital experiences for websites, mobile 
                         applications, B2B and SaaS digital products.
                     </p>
-                    <button className="button primary">Schedule a call</button>
-                    
+                    {/* <button className="button primary">Schedule a call</button> */}
+                     <PopupComponent/>
                 </div>
                 <div className="col imageWrap">
                     <img className="heroImage" src={heroImage}></img>
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 import './header.css'
+import { PopupButton, PopupWidget} from "react-calendly"
 
 class Header extends Component {
+    
     state={open:false}
     handleClick=()=>{
         this.setState({open:!this.state.open})
     }
+
+    PopupComponent = () => {
+        return (
+          <div className="popup-button">
+            <PopupButton className="button primary headerButton"
+              url="https://calendly.com/uxjulija/kickoff-meeting" 
+              rootElement={document.getElementById('root')}
+              text="Schedule a call"
+            />
+          </div>
+        );
+      };
     render() {
         return (
-            
+
             <header className="siteHeader">
                 <section className="container headerContainer">
                     <a className="headerLogo">Julija Jelicanin</a>
@@ -17,7 +31,8 @@ class Header extends Component {
                             <a className="navLink">Work</a>
                             <a className="navLink">About me</a>
                             <a className="navLink">Contact</a>
-                            <button className="button primary">Schedule a call</button>
+                            {/* <button className="button primary">Schedule a call</button> */}
+                            <this.PopupComponent/>
                         </nav>
                         <div class='mobileToggle open' onClick={this.handleClick}>
                             <i id='bar' className={this.state.open ? 'fas fa-times' : 'fas fa-bars'}></i>
